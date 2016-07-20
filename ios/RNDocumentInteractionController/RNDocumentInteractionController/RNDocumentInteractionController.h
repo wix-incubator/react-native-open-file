2b626fcf006ef34dcd739d8ae5e6dae0562bbc8b
Aaron Greenwald <aarong@wix.com>
2016-07-20 11:10:58 +0300

    fix git submodule issues
    add the files properly

---
+++ ios/RNDocumentInteractionController/RNDocumentInteractionController/RNDocumentInteractionController.h
@@ -0,0 +1,15 @@
+//
+//  RNDocumentInteractionController.h
+//  RNDocumentInteractionController
+//
+//  Created by Aaron Greenwald on 7/5/16.
+//  Copyright © 2016 Wix.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+
+@interface RNDocumentInteractionController : NSObject <RCTBridgeModule>
+
+@end
+
---
+++ ios/RNDocumentInteractionController/RNDocumentInteractionController/RNDocumentInteractionController.m
@@ -0,0 +1,30 @@
+//
+//  RNDocumentInteractionController.m
+//  RNDocumentInteractionController
+//
+//  Created by Aaron Greenwald on 7/5/16.
+//  Copyright © 2016 Wix.com. All rights reserved.
+//
+
+#import "RNDocumentInteractionController.h"
+#import <UIKit/UIKit.h>
+
+@implementation RNDocumentInteractionController
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(open: (NSURL *)path)
+{
+    UIDocumentInteractionController *interactionController = [UIDocumentInteractionController interactionControllerWithURL:path];
+    interactionController.delegate = self;
+    [interactionController presentPreviewAnimated:YES];
+}
+
+- (UIViewController *) documentInteractionControllerViewControllerForPreview: (UIDocumentInteractionController *) controller
+{
+    return [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+}
+
+
+@end
+
